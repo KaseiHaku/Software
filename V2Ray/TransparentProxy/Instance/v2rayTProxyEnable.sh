a69a4b09e8d030b9f2225882fdc97bb3ca7cffa6
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2020-05-17 02:13:58 +0800

    Update v2rayTProxyEnable.sh

--- V2Ray/TransparentProxy/Instance/v2rayTProxyEnable.sh
+++ V2Ray/TransparentProxy/Instance/v2rayTProxyEnable.sh
@@ -14,11 +14,11 @@
 # todo 新版本，基于 tproxy 方式的透明代理
 # 代理局域网设备
 iptables -t mangle -N V2RAY
-iptables -t mangle -A V2RAY -d 127.0.0.1/32 -j RETURN
-iptables -t mangle -A V2RAY -d 224.0.0.0/4 -j RETURN 
-iptables -t mangle -A V2RAY -d 255.255.255.255/32 -j RETURN 
-iptables -t mangle -A V2RAY -d 192.168.0.0/16 -p tcp -j RETURN # 直连局域网，避免 V2Ray 无法启动时无法连网关的 SSH，如果你配置的是其他网段（如 10.x.x.x 等），则修改成自己的
-iptables -t mangle -A V2RAY -d 192.168.0.0/16 -p udp ! --dport 53 -j RETURN # 直连局域网，53 端口除外（因为要使用 V2Ray 的 
+iptables -t mangle -A V2RAY -d 127.0.0.0/8 -j RETURN        # 回环地址
+iptables -t mangle -A V2RAY -d 10.0.0.0/8 -j RETURN         # 私有地址
+iptables -t mangle -A V2RAY -d 172.16.0.0/12 -j RETURN      # 私有地址
+iptables -t mangle -A V2RAY -d 192.168.0.0/16 -p tcp ! --dport 53,5353 -j RETURN # 直连局域网，53 端口除外（因为要使用 V2Ray 解析 DNS），避免 V2Ray 无法启动时无法连网关的 SSH，如果你配置的是其他网段（如 10.x.x.x 等），则修改成自己的
+iptables -t mangle -A V2RAY -d 192.168.0.0/16 -p udp ! --dport 53,5353 -j RETURN # 直连局域网，53 端口除外（因为要使用 V2Ray 解析 DNS）
 iptables -t mangle -A V2RAY -m set --match-set vps4 dst -j RETURN    # 如果目标 ip 地址在名为 vps4 的 ipset 中，那么放行
 iptables -t mangle -A V2RAY -p udp -j TPROXY --on-port 12345 --tproxy-mark 1 # 给 UDP 打标记 1，转发至 12345 端口
 iptables -t mangle -A V2RAY -p tcp -j TPROXY --on-port 12345 --tproxy-mark 1 # 给 TCP 打标记 1，转发至 12345 端口
@@ -26,12 +26,13 @@ iptables -t mangle -A PREROUTING -j V2RAY # 应用规则
 
 # 代理网关本机
 iptables -t mangle -N V2RAY_MASK 
-iptables -t mangle -A V2RAY_MASK -d 224.0.0.0/4 -j RETURN 
-iptables -t mangle -A V2RAY_MASK -d 255.255.255.255/32 -j RETURN 
-iptables -t mangle -A V2RAY_MASK -d 192.168.0.0/16 -p tcp -j RETURN # 直连局域网
-iptables -t mangle -A V2RAY_MASK -d 192.168.0.0/16 -p udp ! --dport 53 -j RETURN # 直连局域网，53 端口除外（因为要使用 V2Ray 的 DNS）
+iptables -t mangle -A V2RAY -d 127.0.0.0/8 -j RETURN        # 回环地址
+iptables -t mangle -A V2RAY -d 10.0.0.0/8 -j RETURN         # 私有地址
+iptables -t mangle -A V2RAY -d 172.16.0.0/12 -j RETURN      # 私有地址
+iptables -t mangle -A V2RAY_MASK -d 192.168.0.0/16 -p tcp ! --dport 53,5353 -j RETURN # 直连局域网，53 端口除外（因为要使用 V2Ray 解析 DNS）
+iptables -t mangle -A V2RAY_MASK -d 192.168.0.0/16 -p udp ! --dport 53,5353 -j RETURN # 直连局域网，53 端口除外（因为要使用 V2Ray 解析 DNS）
 iptables -t mangle -A V2RAY_MASK -m set --match-set vps4 dst -j RETURN    # 如果目标 ip 地址在名为 vps4 的 ipset 中，那么放行
-iptables -t mangle -A V2RAY_MASK  -m mark --mark 255 -j RETURN    # 直连 SO_MARK 为 0xff 的流量(0xff 是 16 进制数，数值上等同与上面V2Ray 配置的 255)，此规则目的是避免代理本机(网关)流量出现回环问题
+iptables -t mangle -A V2RAY_MASK -m mark --mark 255 -j RETURN    # 直连 SO_MARK 为 0xff 的流量(0xff 是 16 进制数，数值上等同与上面V2Ray 配置的 255)，此规则目的是避免代理本机(网关)流量出现回环问题
 iptables -t mangle -A V2RAY_MASK -p udp -j MARK -set-mark 1   # 给 UDP 打标记,重路由
 iptables -t mangle -A V2RAY_MASK -p tcp -j MARK --set-mark 1   # 给 TCP 打标记，重路由
 iptables -t mangle -A OUTPUT -j V2RAY_MASK # 应用规则))
