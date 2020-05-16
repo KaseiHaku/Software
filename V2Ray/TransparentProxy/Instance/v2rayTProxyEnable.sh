047effe39ca03142c1150a4cf1bde7f7e954c6b4
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2020-05-16 23:43:30 +0800

    Update v2rayTProxyEnable.sh

--- V2Ray/TransparentProxy/Instance/v2rayTProxyEnable.sh
+++ V2Ray/TransparentProxy/Instance/v2rayTProxyEnable.sh
@@ -31,8 +31,8 @@ iptables -t mangle -A V2RAY_MASK -d 255.255.255.255/32 -j RETURN
 iptables -t mangle -A V2RAY_MASK -d 192.168.0.0/16 -p tcp -j RETURN # 直连局域网
 iptables -t mangle -A V2RAY_MASK -d 192.168.0.0/16 -p udp ! --dport 53 -j RETURN # 直连局域网，53 端口除外（因为要使用 V2Ray 的 DNS）
 iptables -t mangle -A V2RAY_MASK -m set --match-set vps4 dst -j RETURN    # 如果目标 ip 地址在名为 vps4 的 ipset 中，那么放行
-iptables -t mangle -A V2RAY_MASK -j RETURN -m mark --mark 0xff    # 直连 SO_MARK 为 0xff 的流量(0xff 是 16 进制数，数值上等同与上面V2Ray 配置的 255)，此规则目的是避免代理本机(网关)流量出现回环问题
-iptables -t mangle -A V2RAY_MASK -p udp -j MARK --set-mark 1   # 给 UDP 打标记,重路由
+iptables -t mangle -A V2RAY_MASK  -m mark --mark 255 -j RETURN    # 直连 SO_MARK 为 0xff 的流量(0xff 是 16 进制数，数值上等同与上面V2Ray 配置的 255)，此规则目的是避免代理本机(网关)流量出现回环问题
+iptables -t mangle -A V2RAY_MASK -p udp -j MARK -set-mark 1   # 给 UDP 打标记,重路由
 iptables -t mangle -A V2RAY_MASK -p tcp -j MARK --set-mark 1   # 给 TCP 打标记，重路由
 iptables -t mangle -A OUTPUT -j V2RAY_MASK # 应用规则))
 
