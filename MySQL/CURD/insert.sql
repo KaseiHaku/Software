36d54299c39a745b010bc2e55a90a59883170e56
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2020-12-01 15:56:28 +0800

    Update insert.sql

--- MySQL/CURD/insert.sql
+++ MySQL/CURD/insert.sql
@@ -19,9 +19,11 @@ from(
   from tb2 t2 join tb3 t3 on t2.col=t3.col and t2.col2=2
 ) as t2;
 
-/* 存在则更新，不存在则插入 */
+/* 存在则更新，不存在则插入 
+ * 语句含义：如果该记录不存在则，插入该条记录，如果存在，则更新 f_market='sh', f_name='浦发银行'
+ * 规则：f_market, f_stockid, f_name 这三列中，必须有一列是 unique 索引的，根据 unique 列判断是否重复
+ */
 insert into t_stock_chg(f_market, f_stockid, f_name) 
-values('sh', '600000', '白云机场')
-on duplicate key update f_market='sh', f_name='浦发银行'; 
--- 语句含义：如果该记录不存在则，插入该条记录，如果存在，则更新 f_market='sh', f_name='浦发银行'
--- 规则：f_market, f_stockid, f_name 这三列中，必须有一列是 unique 索引的，根据 unique 列判断是否重复
+values('sh', '600000', '白云机场'),('tz', '12324', '台州路桥机场')
+on duplicate key update f_market=values(f_market), f_name=values(f_name);  -- values()  引用 values 中指定字段的值
+
