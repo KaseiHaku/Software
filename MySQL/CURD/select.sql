f6873cc188972964ce929d0d37a9ffd3dd7d07dd
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2021-03-07 16:43:25 +0800

    Update select.sql

--- MySQL/CURD/select.sql
+++ MySQL/CURD/select.sql
@@ -1,3 +1,4 @@
+
 /* MySQL 表连接类型 */
 from tb1, tb2 on                -- 等价于 inner join
 from tb1 full join tb2 on       -- 不支持，使用 left join union right join 替代
@@ -27,56 +28,56 @@ where t2.id is not null;
 
 
 -- group by 后，获取没有 group by 的列
-	select A.col0
-	from t1 as A 
-		inner join (
-			select col1, col2
-			from t1
-			group by col1, col2) as B
-		on (A.col1=B.col1 and A.col2=B.col2);
+    select A.col0
+    from t1 as A 
+        inner join (
+            select col1, col2
+            from t1
+            group by col1, col2) as B
+        on (A.col1=B.col1 and A.col2=B.col2);
 
 -- 从一张表中选择数据插入到另一张表中，如果插入数据要对应，那么通过表连接生成新的表即可
-	insert into target_table(col1, col2, col3)
-	select col4, col5, col6
-	from source_table
-	where ;
+    insert into target_table(col1, col2, col3)
+    select col4, col5, col6
+    from source_table
+    where ;
 
 -- 删除符合子查询条件的行
-	-- 方案一：最佳
-	create table temp (col1 int(9)); -- 先建立中间表
+    -- 方案一：最佳
+    create table temp (col1 int(9)); -- 先建立中间表
 
-	insert into temp
-	select 
-	from t1
-	where;
+    insert into temp
+    select 
+    from t1
+    where;
 
 
-	delete from t1 as A
-	where A.id in (
-		select col1
-		from temp
-		where 
-	);
-	
-	-- 方案二 次佳
-	DELETE e.*
-	FROM tableE e
-	WHERE id IN (SELECT id
-	             FROM (SELECT id
-	                   FROM tableE
-	                   WHERE arg = 1 AND foo = 'bar') as x);
-	-- 方案三                   
-	DELETE th
-	FROM term_hierarchy AS th
-	WHERE th.parent = 1015 AND th.tid IN (
-		    SELECT DISTINCT(th1.tid)
-		    FROM term_hierarchy AS th1
-		    INNER JOIN term_hierarchy AS th2 ON (th1.tid = th2.tid AND th2.parent != 1015)
-		    WHERE th1.parent = 1015
-		);
-		
-	-- 方案四
-	DELETE FROM `secure_links` WHERE `secure_links`.`link_id` IN 
+    delete from t1 as A
+    where A.id in (
+        select col1
+        from temp
+        where 
+    );
+    
+    -- 方案二 次佳
+    DELETE e.*
+    FROM tableE e
+    WHERE id IN (SELECT id
+                 FROM (SELECT id
+                       FROM tableE
+                       WHERE arg = 1 AND foo = 'bar') as x);
+    -- 方案三                   
+    DELETE th
+    FROM term_hierarchy AS th
+    WHERE th.parent = 1015 AND th.tid IN (
+            SELECT DISTINCT(th1.tid)
+            FROM term_hierarchy AS th1
+            INNER JOIN term_hierarchy AS th2 ON (th1.tid = th2.tid AND th2.parent != 1015)
+            WHERE th1.parent = 1015
+        );
+        
+    -- 方案四
+    DELETE FROM `secure_links` WHERE `secure_links`.`link_id` IN 
             (
             SELECT
                 `sl1`.`link_id` 
@@ -105,84 +106,84 @@ where (a.peopleId,a.seq) in (select peopleId,seq from vitae group by peopleId,se
 -- 删除表中多余的重复记录，多列
 delete from vitae a   
 where (a.peopleId, a.seq) in (
-		select peopleId, seq 
-		from vitae 
-		group by peopleId,seq having count(*) > 1                 
-		)   
-	and rowid not in (
-		select min(rowid) 
-		from vitae 
-		group by peopleId,seq having count(*)>1
-	);
+        select peopleId, seq 
+        from vitae 
+        group by peopleId,seq having count(*) > 1                 
+        )   
+    and rowid not in (
+        select min(rowid) 
+        from vitae 
+        group by peopleId,seq having count(*)>1
+    );
 
 
 /*############################################### Row and Column Convert : Begin ####################################################*/
--- 行转列，指把同一行中的不同列，转化到一个列中去
-create table row2col(
-	id int(8) auto_increment primary key,
-	name varchar(8),
-    gender varchar(8),
-    chinese varchar(8),
-    meth varchar(8),
-    english varchar(8)
-);
-insert into `new2016`.`row2col` (`name`, `gender`, `chinese`, `math`, `english`) values ('小明', '男', '44', '54', '64');
-insert into `new2016`.`row2col` (`name`, `gender`, `chinese`, `math`, `english`) values ('小红', '女', '66', '76', '86');
-insert into `new2016`.`row2col` (`name`, `gender`, `chinese`, `math`, `english`) values ('小皮', '男', '78', '88', '98');
+-- 合并 多列 到 一列, MySQL 8 以上支持 with as 语法: select version();
+with aa as ()
+select * from aa;
+                                
+-- 列转行: 把同一行的 三列数据, 变成三行数据
+with 
+  aa as (
+      select '小明' name, '男' gender, '44' chinese, '54' math, '64' english from dual union 
+      select '小红' name, '女' gender, '66' chinese, '76' math, '86' english from dual union
+      select '小皮' name, '男' gender, '78' chinese, '88' math, '98' english from dual
+  )
+select nam, gender, '语文' subject, chinese score into templateTable from aa
+union
+select nam, gender, '数学' subject, math score into templateTable from aa
+union
+select nam, gender, '英语' subject, english score into templateTable from aa
+;
 
--- 基本思路：多次调用 select into 语句到中间表
+
+-- 行转列: 指定一列, 把该列中相同值的行归为一类, 再指定另一列, 将该列中不同的值, 变成表中的一列 
+-- 方法1 
+with 
+  aa as (
+    select '小明' name, '男' gender, 'chinese' subject, '34' score from dual union
+    select '小明' name, '男' gender, 'math' subject, '34' score from dual union
+    select '小明' name, '男' gender, 'english' subject, '34' score from dual union
+    select '小红' name, '女' gender, 'chinese' subject, '34' score from dual union
+    select '小皮' name, '男' gender, 'chinese' subject, '34' score from dual 
+  )
 select 
-	name,
-	gender,
-	'语文',
-	score
-into templateTable
-from row2col;
-
-
--- 列转行，指把一个列中的不同行且不相同的数据，分配到不同的列上去
-create table col2row(
-	id int(8) auto_increment primary key,
-	name varchar(8),
-    gender varchar(8),
-    subject varchar(8),
-    score varchar(8)
-);
-insert into col2row(name, gender, subject, score) values('小明', '男', 'chinese', '34');
-insert into col2row(name, gender, subject, score) values('小明', '男', 'math', '34');
-insert into col2row(name, gender, subject, score) values('小明', '男', 'english', '34');
-insert into col2row(name, gender, subject, score) values('小红', '女', 'chinese', '34');
-insert into col2row(name, gender, subject, score) values('小皮', '男', 'chinese', '34');
-
--- 方法1
-select
-    name,
-    gender,
-    sum(if(subject='chinese', score, 0)) as '语文',
-    sum(if(subject='math', score, 0)) as '数学',
-    sum(if(subject='english', score, 0)) as '英语'
-from col2row
+  name,
+  gender,
+  sum(if(subject='chinese', score, 0)) as '语文',
+  sum(if(subject='math', score, 0)) as '数学',
+  sum(if(subject='english', score, 0)) as '英语'
+from aa
 group by name, gender;
 
+
 -- 方法2，推荐
+with 
+  aa as (
+    select '小明' name, '男' gender, 'chinese' subject, '34' score from dual union
+    select '小明' name, '男' gender, 'math' subject, '34' score from dual union
+    select '小明' name, '男' gender, 'english' subject, '34' score from dual union
+    select '小红' name, '女' gender, 'chinese' subject, '34' score from dual union
+    select '小皮' name, '男' gender, 'chinese' subject, '34' score from dual 
+  )
 select
-	name,
+    name,
     gender,
     max(case subject when 'chinese' then score else 0 end) as '语文',
     max(case subject when 'math' then score else 0 end) as '数学',
     max(case subject when 'english' then score else 0 end) as '英语'
-from col2row
+from aa
 group by name, gender;
 
 /*############################################### Row and Column Convert : End ####################################################*/
 
 /* mysql 函数
-	使用函数的时候要先清楚一件事，
-	select name, colvar from tab; 
-	其中的 name 和 colvar 是列值变量，可以存取列值的.
-	在不同的上下文中有不同的含义，如：
-	在 max(name);  count(name); sum(name); 等函数中，name 相当于 Java 中的 List<name> name 变量；
-	在 if(name='Kasei', 1, 0);  case name when 'Kasei' then 1 else 0 end; 中 name 相当于Java 中 String name; 即直接表示列值
+    使用函数的时候要先清楚一件事，
+    select name, colvar from tab; 
+    其中的 name 和 colvar 是列值变量，可以存取列值的.
+    在不同的上下文中有不同的含义，如：
+    在 max(name);  count(name); sum(name); 等函数中，name 相当于 Java 中的 List<name> name 变量；
+    在 if(name='Kasei', 1, 0);  case name when 'Kasei' then 1 else 0 end; 中 name 相当于Java 中 String name; 即直接表示列值
 */
 if(exp1, exp2, exp3)  -- 如果 exp1 等于 ture，则返回 exp2，否则返回 exp3
 
@@ -195,9 +196,8 @@ partition(partition_name1, partition_name2, partition_name3)
 
 
 /* 常用 sql */
-select 	
+select     
     max(case roh when 1 then COLUMN_NAME else 0 end) as z1
 from 
 (select @rowno:=@rowno+1 roh, COLUMN_NAME from information_schema.COLUMNS, (select @rowno:=0) t2 where table_name = 'discipline_school') t3;
-
 
