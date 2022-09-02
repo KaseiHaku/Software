c1c1d2b725ea81efe2df97bb4dc0375af3a33c93
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2022-09-02 17:22:08 +0800

    Update troubleshoot.sh

--- JDK/troubleshoot.sh
+++ JDK/troubleshoot.sh
@@ -14,6 +14,53 @@ CPU 爆高原因:
         死锁不会引起 CPU 爆高
         线程 WAITING, BLOCKED 状态是不会消耗 CPU 资源的
 
+Native Thread Status: 系统原生线程状态
+    deadlock: 死锁，一般指多个线程调用期间进入了相互资源占用，导致一直等待无法释放的情况。
+    runnable: 正在执行, 该线程占用了资源，正在处理某个操作，如通过SQL语句查询数据库、对某个文件进行写入等
+    blocked: 阻塞状态，指当前线程执行过程中，所需要的资源长时间等待却一直未能获取到，被容器的线程管理器标识为阻塞状态，可以理解为等待资源超时的线程
+    waiting on condition: 线程正处于等待资源或等待某个条件的发生，具体的原因需要结合下面堆栈信息进行分析
+        （1）如果堆栈信息明确是应用代码，则证明该线程正在等待资源，一般是大量读取某种资源且该资源采用了资源锁的情况下，线程进入等待状态，等待资源的读取，或者正在等待其他线程的执行等。
+        （2）如果发现有大量的线程都正处于这种状态，并且堆栈信息中得知正等待网络读写，这是因为网络阻塞导致线程无法执行，很有可能是一个网络瓶颈的征兆：
+            网络非常繁忙，几乎消耗了所有的带宽，仍然有大量数据等待网络读写；
+            网络可能是空闲的，但由于路由或防火墙等原因，导致包无法正常到达；
+            所以一定要结合系统的一些性能观察工具进行综合分析，
+            比如 netstat 统计单位时间的发送包的数量，看是否很明显超过了所在网络带宽的限制；
+            观察CPU的利用率，看系统态的CPU时间是否明显大于用户态的CPU时间。这些都指向由于网络带宽所限导致的网络瓶颈。
+        （3）还有一种常见的情况是该线程在 sleep，等待 sleep 的时间到了，将被唤醒。
+    waiting for monitor entry :
+        Moniter 是Java中用以实现线程之间的互斥与协作的主要手段，它可以看成是对象或者class的锁，每个对象都有，也仅有一个 Monitor。
+        synchronized(obj) { ... }
+    in Object.wait():
+         等待 obj.notify() 或 obj.notifyAll() 方法执行的 线程
+
+JVM Thread Status：JVM 线程运行状态
+    NEW: 
+        至今尚未启动的线程的状态。
+        线程刚被创建，但尚未启动
+    RUNNABLE: 
+        可运行线程的线程状态。
+        线程正在JVM中执行，有可能在等待操作系统中的其他资源，比如处理器
+    BLOCKED: 
+        受阻塞并且正在等待监视器的某一线程的线程状态。
+        处于受阻塞状态的某一线程正在等待 Monitor 锁，以便进入一个同步的块/方法，或者在调用 Object.wait 之后再次进入同步的块/方法
+        在Thread Dump日志中通常显示为 java.lang.Thread.State: BLOCKED (on object monitor)
+    WAITING:
+        某一等待线程的线程状态。
+        线程正在无期限地等待另一个线程来执行某一个特定的操作，线程因为调用下面的方法之一而处于等待状态：
+            不带超时的 Object.wait 方法，日志中显示为 java.lang.Thread.State: WAITING (on object monitor)
+            不带超时的 Thread.join 方法，LockSupport.park 方法，日志中显示为 java.lang.Thread.State: WAITING (parking)
+    TIMED_WAITING:
+        指定了等待时间的某一等待线程的线程状态。
+        线程正在等待另一个线程来执行某一个特定的操作，并设定了指定等待的时间，线程因为调用下面的方法之一而处于定时等待状态：
+            Thread.sleep 方法
+            指定超时值的 Object.wait 方法
+            指定超时值的 Thread.join 方法
+            LockSupport.parkNanos
+            LockSupport.parkUntil
+    TERMINATED:
+        线程处于终止状态。
+
+
 ################################ 坑 ################################
 cap_add: [SYS_PTRACE]               # docker 部署，如果不加这个配置，jstack, jmap 等工具无法使用
 -XX:+DisableAttachMechanism         # 如果启动参数包含这个，jstack, jmap 等工具无法使用
