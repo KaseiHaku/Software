c1dc2a85774344da5d7102d81102587d78f6a68f
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2022-11-30 10:14:03 +0800

    Create 网速测试.sh

---
+++ Linux/ConcreteApply/网速测试.sh
@@ -0,0 +1,11 @@
+shell> npm install --global fast-cli
+shell> fast -u
+
+
+shell> yum -y install iperf3
+shell> iperf -s             # 启动服务端
+shell> iperf -c 192.168.1.2     # 测试到服务端的带宽
+
+
+
+shell>  pip3 install speedtest-cli
