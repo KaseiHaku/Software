4e0be7a10f01172eeb1e5dbea887be7408edf5f1
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2024-06-10 00:48:35 +0800

    Create 全局代理.sh

---
+++ Linux/ConcreteApply/全局代理.sh
@@ -0,0 +1,10 @@
+# HTTP 代理
+export http_proxy=http://192.168.0.106:12345
+export https_proxy=https://192.168.0.106:12345
+
+# Socks5 代理
+export http_proxy=socks5://192.168.0.106:12346
+export https_proxy=socks5://192.168.0.106:12346
+
+# 全部走该代理，不分协议
+export all_proxy=socks5://192.168.0.106:12346
