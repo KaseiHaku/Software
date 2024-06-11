4bec7b60c69b7a9e407d8348ce35c63374005d20
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2024-06-12 05:36:45 +0800

    Update disableV2rayTProxy.sh

--- V2Ray/TransparentProxy/nftables/disableV2rayTProxy.sh
+++ V2Ray/TransparentProxy/nftables/disableV2rayTProxy.sh
@@ -3,7 +3,7 @@
 nft flush ruleset
 
 ip rule del fwmark 2 pref 1024 table 100
-ip route del local 0.0.0.0/0 table 100 dev lo onlink
+ip route del table 100 local 0.0.0.0/0  dev lo onlink
 
 ip -6 rule del fwmark 2 pref 1024 table 100
-ip -6 route del local ::/0 table 100 dev lo onlink
+ip -6 route del table 100 local ::/0 dev lo onlink
