4ae2647d8ccb057223bff7bbf6dd0f5e03f64843
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2024-06-11 16:06:24 +0800

    Update Manual.sh

--- Linux/network/Manual.sh
+++ Linux/network/Manual.sh
@@ -1,3 +1,28 @@
+网络配置方法 {
+    直接修改配置文件 {
+        /etc/sysconfig/network-scripts/ifcfg-ethX
+        /etc/sysconfig/network-scripts/route-ethX
+    }
+    
+    # CentOS 8 以后版本只支持这种方式
+    NetworkManager {
+        shell> nmcli            # 管理网络
+    }
+}
+
+    
+   
+网卡/协议栈 架构 {
+    协议栈：netfilter 运行在 协议栈 中
+        │
+        ├─ 网卡 1
+        ├─ 网卡 2
+        └─ Bridge 1
+    
+
+}
+
+
 ################################ 网络管理命令
 # 测试目的主机连通性
     shell> /proc/sys/net/ipv4/icmp_echo_ignore_all              # 查看当前 ping 是否关闭，0 允许，1 关闭
