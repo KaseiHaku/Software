1d90ed43a827483820ed32eb1ef2437c195dcae8
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2024-10-14 02:00:56 +0800

    Update Security.sh

--- Linux/ConcreteApply/Security.sh
+++ Linux/ConcreteApply/Security.sh
@@ -24,3 +24,18 @@ shell> systemctl restart sshd            # 重启 ssh 服务端
 # 免密登陆
 shell> ssh-keygen        # client 端生成 密钥对; pubkey 一般保存在 ~/.ssh/id_rsa.pub 目录中; id 文件一般保存在 ~/.ssh/id_rsa 文件中
 shell> ssh-copy-id -l root -p 22 192.168.0.200        # 将公钥发送到 server 端
+
+
+
+# Troubleshoot
+shell> alias -p                        # 检查是否有异常的 别名
+shell> less /var/log/auth.log            # 查看异常认证信息
+shell> less /var/log/secure                # ditto
+shell> ss -lnep                         # 检查异常 port
+shell> lsof -p PID                    # 查看指定 PID 打开的所有文件
+shell> less /etc/passwd         # 排查异常账户
+shell> less /etc/shadow        # 排查异常账户
+shell>  # 排查定时任务
+shell> echo $PATH # 检查常用命令是否被修改过; /usr/bin /usr/sbin
+shell> rpm -VF /usr/bin/*        # 验证该目录下的文件是否被修改过，如果被修改过则需要恢复; CentOS 中
+
