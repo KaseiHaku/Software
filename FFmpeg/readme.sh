1b28f37a1269431c7176839d5a8f3a02a00cead1
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2024-10-14 23:36:12 +0800

    Create readme.sh

---
+++ FFmpeg/readme.sh
@@ -0,0 +1,15 @@
+################################ Help ########################
+ffmpeg -h
+ffmpeg -h long
+ffmpeg -h full
+ffmpeg -h type=name        # name=decoder/encoder/demuxer/muxer/filter/bsf/protocol
+ffmpeg -formats            # 指定类别的帮助
+
+
+################################ Help ########################
+
+
+
+
+
+
