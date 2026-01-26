350bc96f234a8c8f4b9f4c27da50a1e898e79d65
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2026-01-27 02:31:08 +0800

    Update fctix.sh

--- Linux/software_installation/Specific/fctix.sh
+++ Linux/software_installation/Specific/fctix.sh
@@ -1,10 +1,22 @@
+# Linux 图形界面协议分两种: 
+#     X11                # 老协议，淘汰中
+#     Wayland            # 新协议
+#     XWayland           # 接收 X11 并转换成 Wayland 协议给操作系统底层
+# Fcitx 设置:
+#     - Start fcitx5 by go to "System settings" -> "Virtual keyboard" -> Select Fcitx 5
+#     - Run chromium/electron application with --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime
 shell> cat <<-'EOF' >> /etc/environment
 # @kasei fcitx5
 INPUT_METHOD=fcitx
-#GTK_IM_MODULE=fcitx
-#QT_IM_MODULE=fcitx
-#SDL_IM_MODULE=fcitx
+# 给 GTK 应用设置输入法，底层使用 X11 协议
+GTK_IM_MODULE=fcitx
+# 给 QT 应用设置输入法，底层使用 X11 协议
+QT_IM_MODULE=fcitx
+# 给 SDL 应用设置输入法，底层使用 X11 协议
+SDL_IM_MODULE=fcitx
+# 给 GLFW 应用设置输入法，底层使用 X11 协议
 GLFW_IM_MODULE=ibus
+# 给使用 Wayland 协议的应用设置输入法
 XMODIFIERS='@im=fcitx'
 
 # @kasei
