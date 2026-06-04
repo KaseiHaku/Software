67d81374d902d418f7c597745a0620353ee6619e
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2026-06-05 03:46:55 +0800

    Update ipv6Tunnel.sh

--- V2Ray/vps/ipv6Tunnel.sh
+++ V2Ray/vps/ipv6Tunnel.sh
@@ -1,3 +1,4 @@
+################################ CentOS ################################
 modprobe ipv6
 ip tunnel add he-ipv6 mode sit remote 66.XXX.XXX.XX local 114.XXX.XXX.119 ttl 255
 ip link set he-ipv6 up
@@ -5,3 +6,15 @@ ip addr add 2001::2/64 dev he-ipv6
 ip route add ::/0 dev he-ipv6
 ip -f inet6 addr
 
+
+
+################################ Debian ################################
+# 添加以下内容到 /etc/network/interfaces:
+auto he-ipv6
+iface he-ipv6 inet6 v4tunnel
+        address 2001::2/64
+        netmask 64
+        endpoint 66.XXX.XXX.XX
+        local 114.XXX.XXX.119
+        ttl 255
+        gateway 2001::1
